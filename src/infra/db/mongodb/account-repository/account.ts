932fb456d7c3911f2acfb6c2d0993ca41bad32cd
bredacoder <bredacoder@gmail.com>
2023-10-31 14:56:41 +0000

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,24 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+
+    const result = await accountCollection.insertOne(accountData)
+    const { _id, name, email, password } = await accountCollection.findOne(
+      result.insertedId,
+    )
+
+    const account = {
+      id: String(_id),
+      name,
+      email,
+      password,
+    }
+
+    return account
+  }
+}
