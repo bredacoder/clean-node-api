f15a8951c09be10db60eec94a41da536581dd6b9
bredacoder <bredacoder@gmail.com>
2023-11-14 09:00:20 +0000

    test: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -26,4 +26,16 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(BadRequest(new MissingParamError('email')))
   })
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(BadRequest(new MissingParamError('password')))
+  })
 })
