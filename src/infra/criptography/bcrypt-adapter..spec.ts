b0bd22ab210778213d644710e6497482138c4f5d
bredacoder <bredacoder@gmail.com>
2023-11-25 21:22:43 +0000

    test: ensure BcryptAdapter throws if compare throws

--- src/infra/criptography/bcrypt-adapter..spec.ts
+++ src/infra/criptography/bcrypt-adapter..spec.ts
@@ -31,7 +31,7 @@ describe('Bcrypt Adapter', () => {
     expect(hash).toBe('hash')
   })
 
-  test('Should throw if bcrypt throws', async () => {
+  test('Should throw if hash throws', async () => {
     const sut = makeSut()
     jest
       .spyOn<any, string>(bcrypt, 'hash')
@@ -66,5 +66,17 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Should throw if compare throws', async () => {
+    const sut = makeSut()
+    jest
+      .spyOn<any, string>(bcrypt, 'compare')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      )
+    const promise = sut.compare('any_value', 'any_hash')
+
+    expect(promise).rejects.toThrow()
   })
 })
