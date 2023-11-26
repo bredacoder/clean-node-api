cc31e2d2fa4881c612e89541dd95200681578c17
bredacoder <bredacoder@gmail.com>
2023-11-26 17:12:36 +0000

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -47,5 +47,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 if login fails', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'daniel.test@gmail.com',
+          password: '123',
+        })
+        .expect(401)
+    })
   })
 })
