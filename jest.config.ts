8ceb14b6913c8e5fae62b0a9ae4517ded715350a
bredacoder <bredacoder@gmail.com>
2023-12-29 14:47:04 +0000

    chore: configure paths mapping

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,16 @@
-import type { Config } from 'jest'
+import type { JestConfigWithTsJest } from 'ts-jest'
 
-const config: Config = {
+const config: JestConfigWithTsJest = {
   roots: ['<rootDir>', '<rootDir>/src'],
   moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '^@data/(.*)$': '<rootDir>/src/data/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+    '^@main/(.*)$': '<rootDir>/src/main/$1',
+    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+    '^@validation/(.*)$': '<rootDir>/src/validation/$1',
+  },
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
