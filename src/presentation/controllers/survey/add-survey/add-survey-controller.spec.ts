f902a89454809f42c43be09b0053da2799ce0853
bredacoder <bredacoder@gmail.com>
2023-12-29 15:51:04 +0000

    test: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,4 +1,7 @@
-import { BadRequest } from '@presentation/helpers/http/http-helper'
+import {
+  BadRequest,
+  InternalServerError,
+} from '@presentation/helpers/http/http-helper'
 import { AddSurveyController } from './add-survey-controller'
 import {
   AddSurvey,
@@ -81,5 +84,18 @@ describe('Add Survey Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut()
+    jest
+      .spyOn(addSurveyStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      )
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(InternalServerError(new Error()))
   })
 })
