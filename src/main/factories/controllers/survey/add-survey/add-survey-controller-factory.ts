9ff43b2bd1bf7fdf2ebd7acbf71f505ac415f222
bredacoder <bredacoder@gmail.com>
2024-02-01 08:07:29 -0400

    refactor: organize main layer folders

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -2,7 +2,7 @@ import { makeLogControllerDecorator } from '@main/factories/decorators/log-contr
 import { Controller } from '@presentation/protocols'
 import { AddSurveyController } from '@presentation/controllers/survey/add-survey/add-survey-controller'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
-import { makeDbAddSurvey } from '@main/factories/usecases/add-survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '@main/factories/usecases/survey/add-survey/db-add-survey-factory'
 
 export const makeAddSurveyController = (): Controller => {
   const surveyController = new AddSurveyController(
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { adaptRoute } from '@main/adapters/express/express-route-adapter'
+import { adaptRoute } from '@main/adapters/express-route-adapter'
 import { makeLoginController } from '@main/factories/controllers/authentication/login/login-controller-factory'
 import { makeSignUpController } from '@main/factories/controllers/authentication/signup/signup-controller-factory'
 
